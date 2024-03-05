978013b70df38f8a4b3f74421d820118f7a3cea0
Marek Kopystyński <marekkopystynski@wp.pl>
2024-03-05 22:30:57 +0100

    Add index.php
    The first index.php file has been added,
    which will be the basis of the project.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>New website</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="icon" type="image/png" href="img/favicon.png">
+	</head>
+	<body>
+
+		<h1>Hello, world!</h1>  
+
+	</body>
+</html>
