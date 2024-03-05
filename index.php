341b41f9c7667b599aaea155105e8afd69928127
Marek Kopystyński <marekkopystynski@wp.pl>
2024-03-05 22:35:26 +0100

    Add layout templete
    A basic layout template has been created.

--- index.php
+++ index.php
@@ -3,13 +3,35 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>New website</title>
+		<title>UEFA Champions League 2023/24 Draw Simulator</title>
 		<link rel="stylesheet" href="style.css">
 		<link rel="icon" type="image/png" href="img/favicon.png">
 	</head>
 	<body>
 
-		<h1>Hello, world!</h1>  
+		<div class="container">
+			<header>
+			
+			</header>
+			<div class="title">
+				<!-- TITLE -->
+			</div>
+			
+			<div class="col-1">
+				<!-- left column -->
+			</div>
+
+			<div class="col-2">
+				<!-- right column -->			 	
+			</div>
+			
+		
+			<footer>
+				Author: Marek Kopystyński | <a href="https://github.com/markop87">Github/markop87</a>
+			</footer>
+		
+		
+		</div>
 
 	</body>
 </html>
