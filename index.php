81a819b317b17f60a9cefd33f1df2eb070013a41
Marek Kopystyński <marekkopystynski@wp.pl>
2024-03-05 22:40:46 +0100

    Add first content
    Add first content to website
    such as the rules of drawing.

--- index.php
+++ index.php
@@ -15,23 +15,40 @@
 			</header>
 			<div class="title">
 				<!-- TITLE -->
+				UEFA Champions League 2023/24 Draw Simulator
 			</div>
 			
 			<div class="col-1">
 				<!-- left column -->
+				<h3>Draw</h3>
 			</div>
 
 			<div class="col-2">
-				<!-- right column -->			 	
+				<!-- right column -->
+				<h3>Draw rules</h3>
+				<p>The 32 teams were drawn into eight groups of four. For the draw, the teams were seeded into four pots, each of eight teams, based on the following principles:</p>
+
+				<p>&raquo; Pot 1 contained the Europa League title holders, and the champions of the top seven associations based on their 2022–23 UEFA country coefficients.</p>
+				<p>&raquo; Pot 2, 3 and 4 contained the remaining teams, seeded based on their 2022–23 UEFA club coefficients.</p>
+				<p>Teams from the same association could not be drawn into the same group. Prior to the draw, UEFA formed pairings of teams from the same association (one pairing for associations with two or three teams, two pairings for associations with four or five teams) based on television audiences, where one team would be drawn into Groups A–D and another team would be drawn into Groups E–H, so that the two teams would play on different days. The following pairings were announced by UEFA after the group stage teams were confirmed:</p>
+				<p>
+				<p>&raquo; Manchester City and Manchester United</p>
+				<p>&raquo; Sevilla and Atlético Madrid</p>
+				<p>&raquo; Barcelona and Real Madrid</p>
+				<p>&raquo; Napoli and Lazio</p>
+				<p>&raquo; Bayern Munich and Borussia Dortmund</p>
+				<p>&raquo; Paris Saint-Germain and Lens</p>
+				<p>&raquo; Benfica and Porto</p>
+				<p>&raquo; Feyenoord and PSV Eindhoven</p>
+				<p>&raquo; Inter Milan and Milan</p>
+				<p>&raquo; RB Leipzig and Union Berlin</p>
+				<p>&raquo; Arsenal and Newcastle United</p>				
 			</div>
 			
-		
 			<footer>
 				Author: Marek Kopystyński | <a href="https://github.com/markop87">Github/markop87</a>
 			</footer>
 		
-		
 		</div>
-
 	</body>
 </html>
